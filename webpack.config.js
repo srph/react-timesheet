55c30d290c108d0b11e878d92e7d70784728228e
srph <seraphipod@gmail.com>
2017-02-04 01:08:26 +0800

    Implement build

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  entry: './lib/index.js',
+
+  output: {
+    path: './dist',
+    filename: process.env.NODE_ENV === 'production'
+      ? 'react-timesheet.min.js'
+      : 'react-timesheet.js'
+  },
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'buble',
+      query: {
+        transforms: {
+          modules: false
+        }
+      }
+    }]
+  }
+
+  // module: {
+  //   loaders: [{
+  //     test: /$\.js/,
+  //     exclude: /node_modules/,
+  //     loader: 'imports-loader?$=jquery,@srph/jqt=>null',
+  //   }]
+  // },
+
+  // externals: [{
+  //   jquery: 'jQuery',
+  //   '@srph/jqt': 'jQuery'
+  // }]
+};
